8fdb5183f3c80c5bc9c39b9df297723ff62a2a6b
erasmo <erasmomontes07@gmail.com>
2019-11-07 13:56:22 -0400

    Add new god to connect to sita_db.

---
+++ oraculo/gods/sita_db.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""Main module."""
+import os
+import requests
+from .base import BaseAPIClient
+from .exceptions import CantAuthenticate, NotSetEnviromentVariable
+
+
+class APIClient(BaseAPIClient):
+    base_url = os.environ.get('SITA_DB_BASE_URL', None)
+
+    _authenticated = False
+
+    def authenticate(self, exception=CantAuthenticate):
+        """
+        Method to authenticate with Faveo.
+        """
+        if not self.base_url:
+            msg = 'You need to put the oracle environment variables.'
+            raise NotSetEnviromentVariable(msg)
+
+        _authenticate_url = self.base_url + 'ping'
+        response = requests.post(
+            _authenticate_url,
+            headers=self._headers_base)
+
+        if response.status_code == 200:
+            self._authenticated = True
+
+        if response.status_code == 403:
+            raise exception('msg')
+
+        return self._authenticated
+
